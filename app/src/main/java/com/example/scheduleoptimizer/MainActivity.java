cbb48f4c9f8473fd83c375e0e083f5d2f2f3bc2d
mikebeau <mikebeau@bu.edu>
2019-12-03 19:36:47 -0500

    using jsoup

--- app/src/main/java/com/example/scheduleoptimizer/MainActivity.java
+++ app/src/main/java/com/example/scheduleoptimizer/MainActivity.java
@@ -6,16 +6,37 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import org.w3c.dom.Text;
+import org.jsoup.nodes.Document;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button openInfo;
     private Button openSavedSchedule;
+    private Button launchBtn;
+    private TextView result;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        result = (TextView) findViewById(R.id.result);
+        launchBtn = (Button) findViewById(R.id.launchBtn);
+        launchBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getWebsite();
+            }
+        });
 
         openSavedSchedule = (Button) findViewById(R.id.openSavedSchedule);
         openSavedSchedule.setOnClickListener(new View.OnClickListener(){
@@ -41,5 +62,40 @@ public class MainActivity extends AppCompatActivity {
     public void openActivitySavedSchedules(){
         Intent intent = new Intent(this, SavedSchedules.class);
         startActivity(intent);
+    }
+
+    public void getWebsite(){
+
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                final StringBuilder builder = new StringBuilder();
+                try {
+                    Document doc = Jsoup.connect("https://www.bu.edu/link/bin/uiscgi_studentlink.pl/1575419340?ModuleName=univschr.pl&SearchOptionDesc=Class+Number&SearchOptionCd=S&KeySem=20203&ViewSem=Fall+2019&College=ENG&Dept=EK&Course=381&Section=").get();
+
+                    String title = doc.title();
+                    Elements links = doc.select("a[href]");
+
+                    builder.append(title).append("\n");
+
+                    for (Element link : links){
+                        builder.append("\n").append("Link: ").append(link.attr("href"))
+                                .append("\n").append("Text: ").append(link.text());
+                    }
+                } catch (IOException e) {
+                    builder.append("Error: ").append(e.getMessage()).append("\n");
+                }
+
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        result.setText(builder.toString());
+                    }
+                });
+            }
+        }).start();
+
+
     }
 }
---
+++ app/src/main/java/webRetriveTest.java
@@ -0,0 +1,11 @@
+import java.io.IOException;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+public class webRetriveTest{
+    public static void main( String[] args ) throws IOException{
+        Document doc = Jsoup.connect("http://www.javatpoint.com").get();
+        String title = doc.title();
+        System.out.println("title is: " + title);
+    }
+}
