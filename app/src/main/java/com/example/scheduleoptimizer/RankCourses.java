4c784bff69e6b661f405992c338e1673fe3f4aaf
knhastie <56002769+knhastie@users.noreply.github.com>
2019-12-12 11:50:05 -0800

    Filled drop-down menus for first page

--- app/src/main/java/com/example/scheduleoptimizer/RankCourses.java
+++ app/src/main/java/com/example/scheduleoptimizer/RankCourses.java
@@ -11,11 +11,22 @@ import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
 
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 public class RankCourses extends AppCompatActivity {
 
     private Button enterRankings;
+
+    private Spinner topChoice;
+    private Spinner Choice2;
+    private Spinner Choice3;
+    private Spinner Choice4;
+    private Spinner Choice5;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,7 +51,44 @@ public class RankCourses extends AppCompatActivity {
             }
     });
 
-}
+        topChoice=findViewById(R.id.CourseChoice1);
+        //Spinner click listeners
+        AdapterView.OnItemSelectedListener listener = topChoice.getOnItemSelectedListener();
+        topChoice.setOnItemSelectedListener(listener);
+        AdapterView.OnItemSelectedListener listener2=Choice2.getOnItemSelectedListener();
+        Choice2.setOnItemSelectedListener(listener2);
+        AdapterView.OnItemSelectedListener listener3=Choice3.getOnItemSelectedListener();
+        Choice3.setOnItemSelectedListener(listener3);
+        AdapterView.OnItemSelectedListener listener4=Choice4.getOnItemSelectedListener();
+        Choice4.setOnItemSelectedListener(listener4);
+        AdapterView.OnItemSelectedListener listener5=Choice5.getOnItemSelectedListener();
+        Choice5.setOnItemSelectedListener(listener5);
+        //Create array adapter
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.testSpinArray, android.R.layout.simple_spinner_item);
+        //Specify layout to use when choices appear
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        //Apply adapter to spinners
+        topChoice.setAdapter(adapter);
+        Choice2.setAdapter(adapter);
+        Choice3.setAdapter(adapter);
+        Choice4.setAdapter(adapter);
+        Choice5.setAdapter(adapter);
+    }
+
+    //User-spinner interactions:
+    public void onItemSelected(AdapterView<?> parent, View view,
+                               int pos, long id) {
+        //Transfers spinner item chosen to string for later use
+        String testSpinResult = parent.getItemAtPosition(pos).toString();
+        //Showing selected spinner item
+        Toast.makeText(parent.getContext(), "Selected: " + testSpinResult, Toast.LENGTH_LONG).show();
+    }
+
+    public void onNothingSelected(AdapterView<?> arg0) {
+        //Action if nothing is selected here
+        String testSpinResult = "@string/noSelection";
+        Toast.makeText(arg0.getContext(), "@string/select", Toast.LENGTH_LONG).show();
+    }
 
     public void openPreferencesMainMenu(){
         Intent intent =new Intent(this,PreferencesMainMenu.class);
